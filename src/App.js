8082ac8009e57e19df1099a2096b0423026fa8cb
Fernando <fernandoperezdelatorremunoz@gmail.com>
2021-08-25 15:26:33 +0200

    Añadido path svg para flecha

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import './App.css';
 import SplitText from './Componentes/SplitText/SplitText';
+import descriptionHomeText from './Componentes/Recursos/descriptionHomeText';
+import Flecha from './Componentes/Recursos/Flecha/flecha';
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
         <div className="home">
-          <div style={{paddingTop: '3%'}}>
+          <div style={{ paddingTop: '3%' }}>
             <h1 className="tituloApp">
               <SplitText copy="Welcome" role="heading" delay="0.5"></SplitText>
             </h1>
             <h1 className="tituloApp">
-            <SplitText copy="to my portfolio" role="heading" delay="0.8"></SplitText>
+              <SplitText copy="to my portfolio" role="heading" delay="0.8"></SplitText>
             </h1>
+            <div className="descripcionHome">
+              <h2 className="descripcionHomeText">{descriptionHomeText}</h2>
+            </div>
+            <div className="flecha">
+              <Flecha/>
+            </div>
           </div>
-          
+
         </div>
         <div className="about">
           <h1>Fernando perez de la torre</h1>
